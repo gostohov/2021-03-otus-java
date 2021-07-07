0764f5bcf205ea1aa9f2ea9ac0aca14f2a10c3c7
gosto <gostohov@gmail.com>
2021-07-07 17:08:34 +0300

    fix: поправил замечания ПР

--- hw03-reflection/src/test/java/myJUnit/MyJUnit.java
+++ hw03-reflection/src/test/java/myJUnit/MyJUnit.java
@@ -4,6 +4,7 @@ import ru.gostohov.annotations.After;
 import ru.gostohov.annotations.Before;
 import ru.gostohov.annotations.Test;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 public class MyJUnit {
 
@@ -16,60 +17,53 @@ public class MyJUnit {
 
         try {
             Object obj = type.getDeclaredConstructor().newInstance();
-            Class<? extends Object> clazz = obj.getClass();
-            Method[] methods = clazz.getMethods();
+            Method[] methods = type.getMethods();
+            ArrayList<Method> beforeMethods = new ArrayList<Method>();
+            ArrayList<Method> testMethods = new ArrayList<Method>();
+            ArrayList<Method> afterMethods = new ArrayList<Method>();
 
-            System.out.println("=========== Test start ======================");
             for (Method method : methods) {
-                try {
-                    if(method.isAnnotationPresent(Test.class)) {
-                        Object newObj = type.getDeclaredConstructor().newInstance();
-                        myJUnit.handleBefore(methods, newObj);
-                        methodCount++;
-                        method.invoke(newObj);
-                        sucCount++;
-                        myJUnit.handleAfter(methods, newObj);
-                    }
-                } catch (Exception e) {
-                    expCount++;
-                    System.out.println(method.getName()+" is Abnormal");
-                    System.out.println("Types of: " + e.getCause().getClass());
-                    System.out.println("the reason: " + e.getCause().getMessage());
-                    System.out.println("---------------");
-                    myJUnit.handleAfter(methods, obj);
+                if (method.isAnnotationPresent(Before.class)) {
+                    beforeMethods.add(method);
+                } else if (method.isAnnotationPresent(Test.class)) {
+                    testMethods.add(method);
+                } else if (method.isAnnotationPresent(After.class)) {
+                    afterMethods.add(method);
                 }
             }
 
-
+            System.out.println("=========== Test start ======================");
+            for (Method testMethod : testMethods) {
+                Object newObj = type.getDeclaredConstructor().newInstance();
+                try {
+                    myJUnit.invokeMethods(beforeMethods, obj);
+                    methodCount++;
+                    testMethod.invoke(newObj);
+                    sucCount++;
+                } catch (Exception e) {
+                    expCount++;
+                    System.out.println(testMethod.getName()+" is Abnormal");
+                    System.out.println("Types of: " + e.getCause().getClass());
+                    System.out.println("the reason: " + e.getCause().getMessage());
+                    System.out.println("---------------");
+                }
+                myJUnit.invokeMethods(afterMethods, newObj);
+            }
             System.out.println("====== Test end =======");
             System.out.println("Test method number:"+methodCount+", Which is abnormal:"+expCount + ", And Normal is:"+sucCount);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void handleBefore(Method[] methods, Object obj) {
+    private void invokeMethods(ArrayList<Method> methods, Object obj) {
         for (Method method : methods) {
             try {
-                if(method.isAnnotationPresent(Before.class)) {
-                    method.invoke(obj);
-                }
+                method.invoke(obj);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-
-    private void handleAfter(Method[] methods, Object obj) {
-        for (Method method : methods) {
-            try {
-                if(method.isAnnotationPresent(After.class)) {
-                    method.invoke(obj);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
 }
