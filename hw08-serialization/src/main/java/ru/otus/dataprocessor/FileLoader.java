137646893ab2462ca50fa6fcd7bfc84e01c6050d
gosto <gostohov@gmail.com>
2021-11-02 18:24:45 +0300

    feat: Выполнил задание
    refs: hw08-serialization

--- hw08-serialization/src/main/java/ru/otus/dataprocessor/FileLoader.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/FileLoader.java
@@ -1,16 +1,36 @@
 package ru.otus.dataprocessor;
 
+import com.google.common.io.Resources;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import ru.otus.model.Measurement;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileLoader implements Loader {
 
+    private final String fileName;
+
     public FileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
         //читает файл, парсит и возвращает результат
-        return null;
+        try {
+            Type listType = new TypeToken<ArrayList<Measurement>>(){}.getType();
+            URL url = Resources.getResource(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(url.getPath()));
+            return new Gson().fromJson(bufferedReader, listType);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
